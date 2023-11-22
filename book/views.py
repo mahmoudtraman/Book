ac444da1a7aa48046a50bd0a7fe38993758740f6
mahmoudtraman <memo24112002@gmail.com>
2023-11-22 08:54:38 -1000

    files in view

--- book/views.py
+++ book/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
-from .models import book 
-# Create your views here.
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
+from rest_framework import generics
+from .seralizers import bookserializer,Authorsserializers
+from .models import book,author
+from rest_framework import status
+from rest_framework.response import Response
+
+ERORR_404_NotFound = {
+    "status": '404 not found',
+    "message": "something went error, this page not found or the object you want has been deleted"
+}
 
 
-def book(request):
-    all_books=book.objects.all()
-    return render(request,'book.html',{'books':all_books})
+class booklistapi(generics.ListAPIView):
+    queryset=book.objects.all()
+    serializer_class=bookserializer
+    filter_backends=[DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+    filterset_fields=['title','publish_date','price']
+    search_fields =['title','price','name']
+    ordering_fields =['price']
+    permission_classes = [IsAuthenticated]
+    
+
+class bookdetailapi(generics.RetrieveUpdateDestroyAPIView):
+    queryset=book.objects.all()
+    serializer_class=bookserializer  
+    
+    
+    
+    
+class Authorlistapi(generics.ListAPIView):
+    queryset=author.objects.all()
+    serializer_class=Authorsserializers
+    filter_backends=[DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+    filterset_fields=['name','birth_date','biography']
+    search_fields =['name']
+    permission_classes = [IsAuthenticated]
+    
+
+class Authordetailapi(generics.RetrieveUpdateDestroyAPIView):
+    queryset=author.objects.all()
+    serializer_class=Authorsserializers  
